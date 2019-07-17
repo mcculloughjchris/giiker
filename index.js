c9b989e85995f005d25565a810a231291b67aa16
Chris McCullough <mcculloughjchris@gmail.com>
2019-07-17 12:29:06 -0400

    remove filter

--- index.js
+++ index.js
@@ -140,9 +140,6 @@ class Giiker extends EventEmitter {
     }
 
     const device = await window.navigator.bluetooth.requestDevice({
-      filters: [{
-        namePrefix: 'GiC',
-      }],
       optionalServices: [SERVICE_UUID, SYSTEM_SERVICE_UUID],
     });
 
